5ae6cb356b8245e2627b916494a01fb424ba1ff0
Raqibur-Rahman <raqibur.edu@gmail.com>
2024-01-11 23:36:09 +0600

    how react add element by div id root

--- ReactInnerWorkingsAndFundamentals/app.js
+++ ReactInnerWorkingsAndFundamentals/app.js
@@ -1,2 +1,8 @@
-console.log("Apollo mission 2");
-console.log(React)
+const App = () => {
+    return React.createElement('h1', { style: { color: "red" } }, "Exploring React Fundamental")
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(App);
